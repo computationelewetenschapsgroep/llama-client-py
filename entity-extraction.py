3c5a64e8c9cdae4bf71ad0e5d2631706984bffb6
Aniket Mitra <aniket.mitra.partner@decathlon.com>
2025-01-25 15:44:27 +0100

    Initial commit
    Signed-off-by: Aniket Mitra <aniket.mitra.partner@decathlon.com>

---
+++ entity-extraction.py
@@ -0,0 +1,126 @@
+## implementation of https://github.com/ggerganov/llama.cpp/blob/master/examples/server/chat.sh in python
+
+import requests
+import re
+import json
+import sys 
+
+from concurrent.futures import ThreadPoolExecutor, as_completed
+
+url = "http://127.0.0.1:8080/"
+
+chat_template_library = [
+    ("sports_category_extraction",[
+    "I want to participate in boston marathon.",
+	"Marathon"
+]),
+ ("location_extraction",[
+    "I want to participate in boston marathon.",
+	"Boston"
+]),
+("product_extraction",[
+    "I want shoes to participate in boston marathon.",
+	"Shoes"
+]),
+("season_extraction",[
+    "I want to participate in boston marathon.",
+	"Spring"
+])
+]
+
+instruction_library =[
+    ("sports_category_extraction","You are a world class entity extractor. For a given user query, extract the sports category from it. No preamble."),
+    ("location_extraction","You are a world class geocoder. For a given user query, extract the location from it. No preamble."),
+    ("product_extraction","You are a world class entity extractor. For a given user query, extract the product category from it. No preamble."),
+    ("season_extraction", "You are a world class season extractor. For a given user query, extract the season from it. No preamble.")
+]
+
+
+
+def get_num_tokens(query)->int:
+    response = requests.post(url+"tokenize",json={"content":query})
+
+    tokens = response.json()
+    num_tokens = len(tokens["tokens"])
+    return num_tokens
+
+def run_concurrent_queries(queries, max_workers=12):
+    """
+    Run the extraction in parallel using multiple threads.
+    """
+    
+    results = []
+    with ThreadPoolExecutor(max_workers=max_workers) as executor:
+        futures = [executor.submit(get_num_tokens, query) for query in queries]
+
+        for future in as_completed(futures):
+            result = future.result()
+            if result:
+                results.append(result)
+    
+    return results
+
+token_count_per_instruction = run_concurrent_queries(map(lambda pair: pair[1],instruction_library))
+
+def get_entities(query, key, chat, instruction, num_tokens):
+    prompt = str()
+    prompt += instruction
+    prompt += " "
+    for item in zip(["\n### Human:","\n### Assistant:"],chat):
+        prompt+=" ".join(item)
+    q = [
+        query,
+        ""
+    ]
+    for item in zip(["\n### Human:","\n### Assistant:"],q):
+         prompt+=" ".join(item)
+
+
+    payload = {"prompt": prompt,
+            "temperature": 0.01,
+            "top_k": 2,
+            "top_p": 0.9,
+            "n_keep": num_tokens,
+            "n_predict": 2,
+            "cache_prompt": True,
+            "stop": ["\n### Human:"],
+            "stream": True}
+
+    response = requests.post(url+"completion",json=payload)
+
+    results = [json.loads(item[5:].strip()) for item in response.text.split("\n") if item and re.match("data:", item)]
+
+    output = ""
+    for item in results:
+        output += item["content"].strip() 
+
+    return (key,output)
+
+def extract_entities_concurrently(query, chats, instructions, token_counts_per_instruction, max_workers=12):
+    keys = []
+    chat_instruction_pair = []
+    for chat, instruction in zip(chats, instructions):
+        k, c = chat
+        k, i = instruction
+        keys.append(k)
+        chat_instruction_pair.append((c,i))
+
+    results = []
+    with ThreadPoolExecutor(max_workers=max_workers) as executor:
+        futures = [executor.submit(get_entities, query, keys[idx], 
+                                   chat , instruction, 
+                                   token_counts_per_instruction[idx]) for idx,(chat , instruction) in enumerate(chat_instruction_pair)]
+
+        for future in as_completed(futures):
+            result = future.result()
+            if result:
+                results.append(result)
+        
+    return results
+
+if __name__ == "__main__":
+    for line in sys.stdin:
+        q = line.strip()
+        entities = extract_entities_concurrently(q, chat_template_library, 
+                                                 instruction_library, token_count_per_instruction)
+        print(entities)
